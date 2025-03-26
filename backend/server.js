a0a04734d8c29539417e0c1caf05305490b6f076
MohammedYaseenkalvium <m.yaseensayed09@gmail.com>
2025-03-26 12:32:46 +0530

    Added MongoDB database connection

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const connectDB = require("./config/db");  
+connectDB();
 
 const app = express();
 
